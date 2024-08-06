5a031af5af53ad58cac367b0e755126577ef916c
Vitus Balazs <Balazs.Vitus@msg.group>
2024-08-06 10:21:27 +0300

    feat: implemented zod validation and created login page

---
+++ apps/ui/src/app/types/users/login.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const emailSchemaCheck = z.string().email('You have to provide a valid email!');
+export const passwordSchemaCheck = z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .max(20, { message: 'Password must be at most 20 characters long' })
+    .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+    .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+    .regex(/[0-9]/, { message: 'Password must contain at least one number' })
+    .regex(/[^a-zA-Z0-9]/, { message: 'Password must contain at least one special character' });
