319f0e2e6029e3ceecabd98596d8be75279fa387
Andrei-David Nan <david.nan@msg.group>
2024-08-06 17:28:18 +0300

    Task 3 channels entity & dto & mapper & repository #20 (#29)

---
+++ apps/api/src/channels/repository/topic.repository.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { Topic } from '../domain/topic.entity';
+
+@Injectable()
+export class TopicRepository {
+    constructor(
+        @InjectRepository(Topic)
+        private readonly repository: Repository<Topic>
+    ) {}
+
+    async findOneById(id: string): Promise<Topic> {
+        return this.repository.findOneBy({ id });
+    }
+
+    async findAll(): Promise<Topic[]> {
+        return this.repository.find();
+    }
+
+    async findByName(name: string): Promise<Topic> {
+        return this.repository.findOneBy({ name });
+    }
+
+    async save(topic: Topic, manager?: EntityManager): Promise<Topic> {
+        if (manager) {
+            return  manager.save(topic);
+        } else {
+            return this.repository.save(topic);
+        }
+    }
+
+    async deleteById(id: string, manager?: EntityManager): Promise<void> {
+      if (manager) {
+        await manager.delete(Topic, id);
+      } else {
+        await this.repository.delete(id);
+      }
+    }
+}
