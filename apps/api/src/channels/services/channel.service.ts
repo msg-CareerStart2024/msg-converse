609a54b1430f171ad882598af16a984186144873
davidnan190 <david.nan@msg.group>
2024-08-07 13:34:20 +0300

    feat: setup transactional channel & topic creation and update

--- apps/api/src/channels/services/channel.service.ts
+++ apps/api/src/channels/services/channel.service.ts
@@ -1,11 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 import { Channel } from '../domain/channel.entity';
 import { ChannelRepository } from '../repository/channel.repository';
-import { EntityManager } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import { TopicService } from './topic.service';
 
 @Injectable()
 export class ChannelService {
-    constructor(private readonly channelRepository: ChannelRepository) {}
+    constructor(
+        private readonly channelRepository: ChannelRepository,
+        private readonly topicService: TopicService,
+        @InjectEntityManager() private readonly entityManager: EntityManager
+    ) {}
 
     async getAll(): Promise<Channel[]> {
         return await this.channelRepository.findAll();
@@ -14,7 +20,52 @@ export class ChannelService {
     async getById(id: string): Promise<Channel> {
         return await this.channelRepository.findOneById(id);
     }
-    // async create(newChannel: Partial<Channel>): Promise<Channel> {}
+
+    async create(newChannelData: Omit<Channel, 'id' | 'createdAt'>): Promise<Channel> {
+        return this.entityManager.transaction(async transactionalEntityManager => {
+            const topicNames = newChannelData.topics.map(topic => topic.name);
+            const topics = await this.topicService.getOrCreateTopics(
+                topicNames,
+                transactionalEntityManager
+            );
+            const newChannel = this.createChannelEntity(newChannelData);
+            newChannel.topics = topics;
+
+            return await this.channelRepository.save(newChannel, transactionalEntityManager);
+        });
+    }
+
+    private createChannelEntity(createChannelDto: Omit<Channel, 'id' | 'createdAt'>): Channel {
+        const newChannel = new Channel();
+        newChannel.name = createChannelDto.name;
+        newChannel.description = createChannelDto.description;
+        return newChannel;
+    }
+
+    async update(id: string, updateChannelData: Partial<Channel>): Promise<Channel> {
+        return this.entityManager.transaction(async transactionalEntityManager => {
+            const existingChannel = await this.channelRepository.findOneById(id);
+
+            if (updateChannelData.name) {
+                existingChannel.name = updateChannelData.name;
+            }
+
+            if (updateChannelData.description !== undefined) {
+                existingChannel.description = updateChannelData.description;
+            }
+
+            if (updateChannelData.topics) {
+                const topicNames = updateChannelData.topics.map(topic => topic.name);
+                const topics = await this.topicService.getOrCreateTopics(
+                    topicNames,
+                    transactionalEntityManager
+                );
+                existingChannel.topics = topics;
+            }
+
+            return await this.channelRepository.save(existingChannel, transactionalEntityManager);
+        });
+    }
 
     async delete(id: string, manager?: EntityManager): Promise<void> {
         await this.channelRepository.deleteById(id, manager);
--- apps/api/src/channels/services/topic.service.ts
+++ apps/api/src/channels/services/topic.service.ts
@@ -5,10 +5,32 @@ import { TopicRepository } from '../repository/topic.repository';
 
 @Injectable()
 export class TopicService {
-    constructor(private topicRepository: TopicRepository) {}
+    constructor(private readonly topicRepository: TopicRepository) {}
 
     async getAll(): Promise<Topic[]> {
         return await this.topicRepository.getAll();
+    }
+
+    async create(topicData: Partial<Topic>, manager?: EntityManager): Promise<Topic> {
+        const topic = new Topic();
+        topic.name = topicData.name;
+        return this.topicRepository.save(topic, manager);
+    }
+
+    async getOrCreateTopics(topicNames: string[], manager: EntityManager): Promise<Topic[]> {
+        const topics: Topic[] = [];
+        for (const topicName of topicNames) {
+            let topic = await this.getByName(topicName);
+            if (!topic) {
+                topic = await this.create({ name: topicName }, manager);
+            }
+            topics.push(topic);
+        }
+        return topics;
+    }
+
+    async getByName(name: string): Promise<Topic> {
+        return await this.topicRepository.findByName(name);
     }
 
     async getById(id: string): Promise<Topic> {
