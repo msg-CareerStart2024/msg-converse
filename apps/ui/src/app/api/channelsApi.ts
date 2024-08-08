07f8a16bd967e40526a6f905d9186a5c46be69cf
Vitus Balazs <Balazs.Vitus@msg.group>
2024-08-08 13:38:13 +0300

    fix: change string to constant in api tags

--- apps/ui/src/app/api/channelsApi.ts
+++ apps/ui/src/app/api/channelsApi.ts
@@ -1,22 +1,42 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { API_URLS, BASE_URL } from '../config/api-config';
+import { setChannels } from '../features/channels/slices/channels-slice';
+import { Channel } from '../types/channels/Channel';
+import { API_CACHE_TAGS } from '../config/api-tags';
 
 export const channelsApi = createApi({
     reducerPath: 'channelsApi',
-    tagTypes: ['Channels'],
+    tagTypes: [API_CACHE_TAGS.CHANNELS],
     baseQuery: fetchBaseQuery({
         baseUrl: BASE_URL
     }),
     endpoints: builder => ({
-        getChannels: builder.query<void, void>({
+        getChannels: builder.query<Channel[], void>({
             query: () => API_URLS.CHANNELS,
-            providesTags: ['Channels']
+            providesTags: [API_CACHE_TAGS.CHANNELS],
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const { data } = await queryFulfilled;
+                    dispatch(setChannels(data));
+                } catch (error) {
+                    alert('Getting channels failed');
+                }
+            }
         }),
-        searchChannels: builder.query<void, string>({
+        searchChannels: builder.query<Channel[], string>({
             query: keyword => `${API_URLS.CHANNELS}/?searchKey=${encodeURIComponent(keyword)}`,
-            providesTags: ['Channels']
+            providesTags: [API_CACHE_TAGS.CHANNELS],
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const { data } = await queryFulfilled;
+                    dispatch(setChannels(data));
+                } catch (error) {
+                    alert('Searching channels failed');
+                }
+            }
         })
     })
 });
 
-export const { useLazyGetChannelsQuery, useLazySearchChannelsQuery } = channelsApi;
+export const { useLazyGetChannelsQuery, useLazySearchChannelsQuery, useGetChannelsQuery } =
+    channelsApi;
