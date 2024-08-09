f89901d20bc88c459eb1f0ebe37eedfee32c4fde
dariusparaschiv5 <Darius.Paraschiv@msg.group>
2024-08-09 12:57:28 +0300

    Task-33: fix: removed unnecessary imports

--- apps/ui/src/app/features/channels/pages/ChannelPage.tsx
+++ apps/ui/src/app/features/channels/pages/ChannelPage.tsx
@@ -4,6 +4,7 @@ import ChannelFormView from '../components/ChannelFormView';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect, useState } from 'react';
 import { ChannelFormValues, channelSchema } from '../../../types/channels/schemas/channel.schema';
+import React from 'react';
 
 export default function ChannelPage() {
     const [channelData, setChannelData] = useState<ChannelFormValues | null>(null);
