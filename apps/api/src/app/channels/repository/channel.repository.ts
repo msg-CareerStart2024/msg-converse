3185e2d5a16d1562aa9c3a7e7861c96f6bd6b514
davidnan190 <david.nan@msg.group>
2024-08-09 16:31:37 +0300

    chore: small filtering fixes, code quality imrpovements;

--- apps/api/src/app/channels/repository/channel.repository.ts
+++ apps/api/src/app/channels/repository/channel.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Brackets, EntityManager, Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { Channel } from '../domain/channel.entity';
 
 @Injectable()
