4135b2c82271ba2da66ed723bbad761e5f8b84a7
Vitus Balazs <Balazs.Vitus@msg.group>
2024-08-19 11:21:16 +0300

    fix: removed functions which don't need implementation

--- apps/api/src/app/messages/gateway/message.gateway.ts
+++ apps/api/src/app/messages/gateway/message.gateway.ts
@@ -3,10 +3,13 @@ import {
     OnGatewayConnection,
     OnGatewayDisconnect,
     OnGatewayInit,
+    SubscribeMessage,
     WebSocketGateway,
     WebSocketServer
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { SocketEvent } from '../enum/socket-event.enum';
+import { UserService } from '../../users/service/user.service';
 
 @WebSocketGateway()
 export class MessageGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
@@ -14,15 +17,46 @@ export class MessageGateway implements OnGatewayInit, OnGatewayConnection, OnGat
     server: Server;
     private logger: Logger = new Logger('MessagesGateway');
 
+    constructor(private userService: UserService) {}
+
     afterInit(): void {
-        this.logger.log(`Server initialized`);
+        this.logger.log('Server initialized');
     }
 
     handleConnection(client: Socket): void {
-        this.logger.log(`Client connected: ${client.id}`);
+        console.log('query:', client.handshake.query);
+        console.log('headers:', client.handshake.headers);
+        console.log('auth:', client.handshake.auth);
+        const accessToken = client.handshake.auth.accessToken;
+        if (!accessToken) {
+            this.logger.error('No authorization token provided!');
+        }
+
+        client.emit(SocketEvent.CONNECTION_ERROR, 'Invalid token');
+        client.disconnect();
     }
 
     handleDisconnect(client: Socket): void {
         this.logger.log(`Client disconnected: ${client.id}`);
+    }
+
+    @SubscribeMessage(SocketEvent.JOIN_CHANNEL_CHAT)
+    handleConnectToChannel(): void {
+        this.logger.log('connect to channel chat');
+    }
+
+    @SubscribeMessage(SocketEvent.LEAVE_CHANNEL_CHAT)
+    handleDisconnectFromChannel(): void {
+        this.logger.log('disconnect from channel chat');
+    }
+
+    @SubscribeMessage(SocketEvent.NEW_MESSAGE)
+    handleNewMessage(): void {
+        this.logger.log('new message');
+    }
+
+    @SubscribeMessage(SocketEvent.PREVIOUS_MESSAGES)
+    handlePreviousMessages(): void {
+        this.logger.log('previous messages');
     }
 }
