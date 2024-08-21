65743fd1b3229c10f0cd8800d4cd4c38f396b1cc
Andrei-David Nan <david.nan@msg.group>
2024-08-21 16:22:45 +0300

    TC Pre-Demo improvements (#157)

--- apps/ui/src/app/features/channels/components/ChannelFormView.tsx
+++ apps/ui/src/app/features/channels/components/ChannelFormView.tsx
@@ -8,7 +8,6 @@ import {
 } from 'react-hook-form';
 
 import { getColor } from '../../../lib/avatar-colors';
-import { Channel } from '../../../types/channel/channel.types';
 import { CHANNEL_FORM_ACTION_TYPE } from '../../../types/channel/ChannelFormActionType.enums';
 import { Topic } from '../../../types/channel/Topic.types';
 import { User } from '../../../types/login/User.types';
@@ -16,6 +15,7 @@ import { UserRole } from '../../../types/login/UserRole.enum';
 import { ChannelFormValues } from '../schemas/ChannelFormValues.schema';
 import ActionButtonView from './ActionButtonView';
 import TopicsView from './TopicsView';
+import { ChannelDTO } from '../../../types/channel/channel.types';
 
 type ChannelFormProps = {
     handleSubmit: UseFormHandleSubmit<ChannelFormValues>;
@@ -24,6 +24,7 @@ type ChannelFormProps = {
     isSubmitting: boolean;
     isValid: boolean;
     isEditForm: boolean;
+    isDirty: boolean;
     getValues: UseFormGetValues<{
         name: string;
         topics: string;
@@ -42,7 +43,7 @@ type ChannelFormProps = {
     currentUser: User;
     handleAddTopic: () => void;
     handleDeleteTopic: (name: string) => void;
-    data?: Channel;
+    data?: ChannelDTO;
 };
 
 export default function ChannelFormView({
@@ -51,6 +52,7 @@ export default function ChannelFormView({
     errors,
     isSubmitting,
     isValid,
+    isDirty,
     isEditForm,
     getValues,
     setValue,
